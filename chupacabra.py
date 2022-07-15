e147720ac47e4cb4626c3e5d2c4b8b49dcfd8fc9
ideyquickvex <109346384+ideyquickvex@users.noreply.github.com>
2022-07-15 13:05:27 +0100

    Add files via upload

---
+++ chupacabra.py
@@ -0,0 +1,11 @@
+while True:
+    answer = input("Guess the word I'm thinking about: ")
+    if answer != "chupacabra":
+        print("Try again")
+        input ("Guess the word I'm thinking about: ")
+    else:
+        pass
+    
+    break
+print("You've successfully left the loop.")
+        
