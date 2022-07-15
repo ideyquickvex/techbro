999198eab56561522f68e044998d7223273f848a
ideyquickvex <109346384+ideyquickvex@users.noreply.github.com>
2022-07-15 12:58:40 +0100

    Add files via upload

---
+++ pretty vowel eater.py
@@ -0,0 +1,18 @@
+wordWithoutVowels = ""
+userWord = input("Enter a word: ")
+userWord = userWord.upper()
+for letter in userWord:
+    if letter == 'A':
+        continue
+    elif letter == 'E':
+        continue
+    elif letter == 'I':
+        continue
+    elif letter == 'O':
+        continue
+    elif letter == 'U':
+        continue
+    else:
+        wordWithoutVowels += letter
+        
+print(wordWithoutVowels)
